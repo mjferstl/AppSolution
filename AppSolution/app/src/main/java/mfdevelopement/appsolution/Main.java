fed8d25e353887069e2c035a8e0ad54f41aa3c1d
mjferstl <39524509+mjferstl@users.noreply.github.com>
2019-06-16 23:37:27 +0200

    store ImageButtons and corresponding activity classes in Lists
    set Imgae buttons width depending on the screen width
    Bug: changing app language not working

--- AppSolution/app/src/main/java/mfdevelopement/appsolution/Main.java
+++ AppSolution/app/src/main/java/mfdevelopement/appsolution/Main.java
@@ -17,13 +17,19 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+
+import mfdevelopement.appsolution.device.general.DisplayData;
 
 public class Main extends AppCompatActivity {
 
@@ -37,6 +43,11 @@ public class Main extends AppCompatActivity {
     private String USERLANGUAGE = "userLanguage";
 
     private Button btn_language_Ok = null;
+
+    private ImageButton btnCocktails, btnMensa, btnGeometry, btnMechEng,
+            btnCurrencyConverter, btnSensors, btnLibrary, btnNews, btnDressSize, btnWeather;
+    private List<ImageButton> imageButtonList = new ArrayList<>();
+    private List<Class> targetActivities = new ArrayList<>();
 
     SharedPreferences userLanguage;
 
@@ -56,97 +67,113 @@ public class Main extends AppCompatActivity {
         initButtons();
     }
 
+    /**
+     * initialize all buttons
+     */
     private void initButtons() {
 
-        // Cochtails
-        ImageButton btnCocktails = findViewById(R.id.btn_main_cocktails);
-        btnCocktails.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(CocktailOverview.class);
-            }
-        });
+        // find image buttons by id
+        btnCocktails = findViewById(R.id.btn_main_cocktails);
+        btnMensa = findViewById(R.id.btn_main_mensa);
+        btnGeometry = findViewById(R.id.btn_main_geometry_calculator);
+        btnMechEng = findViewById(R.id.btn_main_mechanical_engineering);
+        btnCurrencyConverter = findViewById(R.id.btn_main_currency_converter);
+        btnSensors = findViewById(R.id.btn_main_sensors);
+        btnLibrary = findViewById(R.id.btn_main_regensburger_katalog);
+        btnNews = findViewById(R.id.btn_main_news);
+        btnDressSize = findViewById(R.id.btn_main_clothes_size);
+        btnWeather = findViewById(R.id.btn_main_weather);
 
-        // Mensa STWNO
-        ImageButton btnMensa = findViewById(R.id.btn_main_mensa);
-        btnMensa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(MensaSpeiseplan.class);
-            }
-        });
 
-        // geometry
-        ImageButton btnGeometry = findViewById(R.id.btn_main_geometry_calculator);
-        btnGeometry.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(GeometryCalculator.class);
-            }
-        });
+        // add image buttons and their target activities to lists
+        imageButtonList.add(btnCocktails);
+        targetActivities.add(CocktailOverview.class);
 
-        // mechanical engineering
-        ImageButton btnMechEng = findViewById(R.id.btn_main_mechanical_engineering);
-        btnMechEng.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(MechanicalEngineering.class);
-            }
-        });
+        imageButtonList.add(btnMensa);
+        targetActivities.add(MensaSpeiseplan.class);
 
-        // currency converter
-        ImageButton btnCurrencyConverter = findViewById(R.id.btn_main_currency_calculator);
-        btnCurrencyConverter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(CurrencyConverter.class);
-            }
-        });
+        imageButtonList.add(btnGeometry);
+        targetActivities.add(GeometryCalculator.class);
 
-        // sensors
-        ImageButton btnSensors = findViewById(R.id.btn_main_sensors);
-        btnSensors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(Sensors.class);
-            }
-        });
+        imageButtonList.add(btnMechEng);
+        targetActivities.add(MechanicalEngineering.class);
 
-        // library OTH and univertiy regensburg
-        ImageButton btnLibrary = findViewById(R.id.btn_main_regensburger_katalog);
-        btnLibrary.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(RegensburgerKatalog.class);
-            }
-        });
+        imageButtonList.add(btnCurrencyConverter);
+        targetActivities.add(CurrencyConverter.class);
 
-        // news
-        ImageButton btnNews = findViewById(R.id.btn_main_news);
-        btnNews.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(News.class);
-            }
-        });
+        imageButtonList.add(btnSensors);
+        targetActivities.add(Sensors.class);
 
-        // dress size
-        ImageButton btnDressSize = findViewById(R.id.btn_main_clothes_size);
-        btnDressSize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(ClothesSize.class);
-            }
-        });
+        imageButtonList.add(btnLibrary);
+        targetActivities.add(RegensburgerKatalog.class);
 
-        // weather
-        ImageButton btnWeather = findViewById(R.id.btn_main_weather);
-        btnWeather.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity(WeatherOverview.class);
+        imageButtonList.add(btnNews);
+        targetActivities.add(null); // News.class
+
+        imageButtonList.add(btnDressSize);
+        targetActivities.add(ClothesSize.class);
+
+        imageButtonList.add(btnWeather);
+        targetActivities.add(WeatherOverview.class);
+
+        // set all onClickListeners
+        for (int i=0; i<imageButtonList.size(); i++) {
+
+            // if no class is specified, no onClickListener is initialized
+            if (targetActivities.get(i) == null) {
+                imageButtonList.get(i).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(Main.this,"Noch keine Funktion hinterlegt ... ",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
-        });
+            else {
+                final Class activity = targetActivities.get(i);
+                imageButtonList.get(i).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openActivity(activity);
+                    }
+                });
+            }
+        }
+
+        changeButtonsSize();
+    }
+
+    private void changeButtonsSize() {
+
+        // create DisplayData Object and get screen sizes in pixels
+        DisplayData displayData = new DisplayData(this);
+        int width = displayData.getWidthPx();
+        int height = displayData.getHeightPx();
+
+        int btnWidth = width/4;
+
+        for (int i=0; i<imageButtonList.size(); i++) {
+            setImageButtonSize(imageButtonList.get(i),btnWidth);
+        }
+    }
+
+    private void setImageButtonSize(ImageButton btn, int width) {
+
+        int height = width;
+
+        // Get the last ImageButton's layout parameters
+        ViewGroup.LayoutParams params = btn.getLayoutParams();
+
+        // Set the height of this ImageButton
+        params.height = height;
+
+        // Set the width of that ImageButton
+        params.width = width;
+
+        // Apply the updated layout parameters to last ImageButton
+        btn.setLayoutParams(params);
+
+        // Set the ImageButton image scale type for fourth ImageButton
+        btn.setScaleType(ImageButton.ScaleType.FIT_XY);
     }
 
     /**
@@ -231,24 +258,28 @@ public class Main extends AppCompatActivity {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.select_language);
 
+        final RadioGroup rg = dialog.findViewById(R.id.rdgr_select_language);
+        final RadioButton rbLangDE = rg.findViewById(R.id.lang_de);
+        final RadioButton rbLangEN = rg.findViewById(R.id.lang_en);
+
         btn_language_Ok = dialog.findViewById(R.id.btn_language_ok);
         btn_language_Ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setLocale(languageChoosen);
+                if (rbLangDE.isChecked()) {
+                    setLocale("de");
+                }
+                else if (rbLangEN.isChecked()) {
+                    setLocale("en");
+                }
+
                 dialog.dismiss();
             }
         });
 
-        RadioGroup rg = dialog.findViewById(R.id.rdgr_select_language);
-
+        // set radio buttons depending on current language selection
         String actLang = getResources().getConfiguration().locale.toString();
-        Log.i(LogTag,actLang);
-        RadioButton rbLangDE = rg.findViewById(R.id.lang_de);
-        RadioButton rbLangEN = rg.findViewById(R.id.lang_en);
-
-        Log.i(LogTag,"Language: actLang = " + actLang);
-
+        Log.i(LogTag,"current language: " + actLang);
         switch (actLang) {
             case "de":
                 rbLangDE.setChecked(true);
@@ -260,27 +291,6 @@ public class Main extends AppCompatActivity {
                 rbLangEN.setChecked(true);
         }
 
-        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                int childCount = group.getChildCount();
-                for (int x = 0; x < childCount; x++) {
-                    RadioButton btn = (RadioButton) group.getChildAt(x);
-                    if (btn.getId() == checkedId) {
-                        Log.i(LogTag,"selected RadioButton ->" + btn.getText().toString());
-                        switch (btn.getId()) {
-                            case R.id.lang_de:
-                                languageChoosen = "de";
-                                break;
-                            case R.id.lang_en:
-                                languageChoosen = "en";
-                                break;
-                        }
-                    }
-                }
-            }
-        });
         dialog.show();
     }
 
@@ -299,6 +309,7 @@ public class Main extends AppCompatActivity {
         } else{
             conf.locale=myLocale;
         }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
             getApplicationContext().createConfigurationContext(conf);
         } else {
@@ -312,4 +323,3 @@ public class Main extends AppCompatActivity {
         Log.i(LogTag,"Language changed to " + lang);
     }
 }
-
