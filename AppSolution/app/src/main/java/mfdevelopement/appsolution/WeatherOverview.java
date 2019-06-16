744ed68231a3dd47a24b21c458431e8278425de7
mjferstl <39524509+mjferstl@users.noreply.github.com>
2019-06-16 13:20:13 +0200

    added title for weather forecast dialog

--- AppSolution/app/src/main/java/mfdevelopement/appsolution/WeatherOverview.java
+++ AppSolution/app/src/main/java/mfdevelopement/appsolution/WeatherOverview.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,30 +44,43 @@ public class WeatherOverview extends AppCompatActivity {
         appname = getString(R.string.app_name);
         LogTag = appname + "/WeatherOverview";
 
+        final String FORECAST = getString(R.string.forecast);
+
         listView = findViewById(R.id.lv_wheater_overview);
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
+                // get weather forecast for selected city
                 Weather selectedWeather = weatherData.get(position);
-                Toast.makeText(WeatherOverview.this, selectedWeather.getCityName(), Toast.LENGTH_LONG).show();
+                String selectedCity = selectedWeather.getCityName();
                 List<WeatherForecast> wf = selectedWeather.getWeatherForecast();
 
-                // open a dialog
-                Log.i(LogTag,"open weather forecast dialog, city " + selectedWeather.getCityName());
+                // create a new dialog containing the weather forecast data
+                Log.i(LogTag,"open weather forecast dialog, city " + selectedCity);
                 final Dialog dialog = new Dialog(WeatherOverview.this);
                 dialog.setContentView(R.layout.dialog_weather_forecast);
-                dialog.setTitle("Wettervorhersage");
+
+                // button to close the dialog
                 Button btnForecastDismiss = dialog.findViewById(R.id.btn_dia_weather_forecast_dismiss);
                 btnForecastDismiss.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         dialog.cancel();
-                        Log.i(LogTag,"Close the forecast dialog");
+                        Log.i(LogTag,"ButtonClick: Close weather forecast dialog");
                     }
                 });
+
+                // fill in items of the listview
                 ListView listViewForecast = dialog.findViewById(R.id.lv_weather_forecast);
                 listViewForecast.setAdapter(new WeatherForecastListAdapter(WeatherOverview.this, wf));
+
+                // set title of dialog
+                TextView tv_title = dialog.findViewById(R.id.tv_dia_weather_forecast_title);
+                String title = selectedCity + " - " + FORECAST;
+                tv_title.setText(title);
+
+                // show dialog
                 dialog.show();
 
                 return true;
--- AppSolution/app/src/main/java/mfdevelopement/appsolution/listview_adapters/WeatherForecastListAdapter.java
+++ AppSolution/app/src/main/java/mfdevelopement/appsolution/listview_adapters/WeatherForecastListAdapter.java
@@ -3,7 +3,6 @@ package mfdevelopement.appsolution.listview_adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,19 +27,21 @@ public class WeatherForecastListAdapter extends ArrayAdapter<WeatherForecast> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
+        // get current weather forecast data
         WeatherForecast currentForecast= getItem(position);
 
+        // choose the adapter for the list view items
         View view = convertView;
          if(view == null) {
              view = LayoutInflater.from(getContext()).inflate(R.layout.adapter_wheater_forecast, parent, false);
          }
 
+         // get fields of each list item
          TextView time = view.findViewById(R.id.tv_weather_forecast_time);
          TextView temperature = view.findViewById(R.id.tv_weather_forecast_temperature);
          ImageView icon = view.findViewById(R.id.img_weather_forecast_icon);
 
-        Log.i("wettervorhersage",currentForecast.getTime());
-
+         // if forecast contains no time, then show error message
          if (currentForecast.getTime() != null) {
              time.setText(currentForecast.getTime());
 
@@ -52,7 +53,7 @@ public class WeatherForecastListAdapter extends ArrayAdapter<WeatherForecast> {
                  icon.setImageResource(currentForecast.getIconId());
              }
          } else {
-             time.setText("x.x.x");
+             time.setText(" - - - ");
              temperature.setText(errorMsgLoading);
              icon.setVisibility(View.INVISIBLE);
          }
