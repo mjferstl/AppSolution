d0fa08219bfbb59d20732ce1ad434109419a2d40
mjferstl <39524509+mjferstl@users.noreply.github.com>
2019-06-16 18:55:22 +0200

    added dialog for choosing cities list

--- AppSolution/app/src/main/java/mfdevelopement/appsolution/WeatherOverview.java
+++ AppSolution/app/src/main/java/mfdevelopement/appsolution/WeatherOverview.java
@@ -5,17 +5,25 @@ import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import mfdevelopement.appsolution.device.general.DisplayData;
 import mfdevelopement.appsolution.listview_adapters.WeatherForecastListAdapter;
 import mfdevelopement.appsolution.listview_adapters.WeatherOverviewListAdapter;
 import mfdevelopement.appsolution.models.Weather;
@@ -23,6 +31,7 @@ import mfdevelopement.appsolution.models.WeatherForecast;
 
 public class WeatherOverview extends AppCompatActivity {
 
+    private String FORECAST;
     private ListView listView = null;
     private List<Weather> weatherData = new ArrayList<>();
 
@@ -41,63 +50,137 @@ public class WeatherOverview extends AppCompatActivity {
 
         appname = getString(R.string.app_name);
         LogTag = appname + "/WeatherOverview";
+        FORECAST = getString(R.string.forecast);
+
+        FloatingActionButton fab = findViewById(R.id.btn_float_weather_overview_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCitiesList();
+                updateWeatherData();
+            }
+        });
 
         String[] owmCityCodes = getResources().getStringArray(R.array.owmCityCodex);
-        for(String value : owmCityCodes) {
-            String[] cityCodePair = value.split(":");
+        List<String> owmCityCodesList = new ArrayList<>(Arrays.asList(owmCityCodes));
+        Collections.sort(owmCityCodesList, new Comparator<String>()
+        {
+            @Override
+            public int compare(String text1, String text2)
+            {
+                return text1.compareToIgnoreCase(text2);
+            }
+        });
+
+        // separate city name and city code and save it to different lists
+        String lastCity = "";
+        for(int i=0; i<owmCityCodesList.size(); i++) {
+            String[] cityCodePair = owmCityCodesList.get(i).split(":");
 
             String cityName = cityCodePair[0].trim();
             int cityCode = Integer.valueOf(cityCodePair[1].trim());
+
+            // remove if duplicate
+            if (cityName.equals(lastCity)) {
+                owmCityCodesList.remove(i);
+                i--;
+                continue;
+            }
+
             cityNames.add(cityName);
             cityCodes.add(cityCode);
+
+            lastCity = cityName;
         }
 
-        saveUserCities();
+        // load the cities for the current user
         userCityCodes = loadUserCities();
 
-        final String FORECAST = getString(R.string.forecast);
-
         listView = findViewById(R.id.lv_wheater_overview);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Weather selectedWeather = weatherData.get(position);
+                showForecastDialog(selectedWeather);
+            }
+        });
+
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-
-                // get weather forecast for selected city
-                Weather selectedWeather = weatherData.get(position);
-                String selectedCity = selectedWeather.getCityName();
-                List<WeatherForecast> wf = selectedWeather.getWeatherForecast();
-
-                // create a new dialog containing the weather forecast data
-                Log.i(LogTag,"open weather forecast dialog, city " + selectedCity);
-                final Dialog dialog = new Dialog(WeatherOverview.this);
-                dialog.setContentView(R.layout.dialog_weather_forecast);
-
-                // button to close the dialog
-                Button btnForecastDismiss = dialog.findViewById(R.id.btn_dia_weather_forecast_dismiss);
-                btnForecastDismiss.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.cancel();
-                        Log.i(LogTag,"ButtonClick: Close weather forecast dialog");
-                    }
-                });
-
-                // fill in items of the listview
-                ListView listViewForecast = dialog.findViewById(R.id.lv_weather_forecast);
-                listViewForecast.setAdapter(new WeatherForecastListAdapter(WeatherOverview.this, wf));
-
-                // set title of dialog
-                TextView tv_title = dialog.findViewById(R.id.tv_dia_weather_forecast_title);
-                String title = selectedCity + " - " + FORECAST;
-                tv_title.setText(title);
-
-                // show dialog
-                dialog.show();
-
+                // return the current item
                 return true;
             }
         });
 
+        updateWeatherData();
+    }
+
+    private void showForecastDialog(Weather weather) {
+
+        // get weather forecast of the selected city
+        String selectedCity = weather.getCityName();
+        List<WeatherForecast> wf = weather.getWeatherForecast();
+
+        // create a new dialog containing the weather forecast data
+        Log.i(LogTag,"open weather forecast dialog, city " + selectedCity);
+        final Dialog dialog = new Dialog(WeatherOverview.this);
+        dialog.setContentView(R.layout.dialog_weather_forecast);
+
+        // button to close the dialog
+        Button btnForecastDismiss = dialog.findViewById(R.id.btn_dia_weather_forecast_dismiss);
+        btnForecastDismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+                Log.i(LogTag,"ButtonClick: Close weather forecast dialog");
+            }
+        });
+
+        // fill in items of the listview
+        ListView listViewForecast = dialog.findViewById(R.id.lv_weather_forecast);
+        listViewForecast.setAdapter(new WeatherForecastListAdapter(WeatherOverview.this, wf));
+
+        // set title of dialog
+        TextView tv_title = dialog.findViewById(R.id.tv_dia_weather_forecast_title);
+        String title = selectedCity + " - " + FORECAST;
+        tv_title.setText(title);
+
+        // change the dialog width to 80% of the screen width
+        LinearLayout layout = dialog.findViewById(R.id.lin_lay_dia_weather_forecast);
+        DisplayData displayData = new DisplayData(WeatherOverview.this);
+        int width = displayData.getWidthPx()*8/10;
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(width,LinearLayout.LayoutParams.WRAP_CONTENT,1);
+        layout.setLayoutParams(params);
+
+        // show dialog
+        dialog.show();
+    }
+
+    private void showCitiesList() {
+
+        final Dialog dialog = new Dialog(WeatherOverview.this);
+        dialog.setContentView(R.layout.dialog_weather_add_city);
+
+        ListView listView = dialog.findViewById(R.id.lv_weather_cities);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cityNames);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                userCityCodes.add(cityCodes.get(position));
+                dialog.dismiss();
+                saveUserCities();
+                updateWeatherData();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void updateWeatherData() {
         new loadWeatherData(this).execute();
     }
 
