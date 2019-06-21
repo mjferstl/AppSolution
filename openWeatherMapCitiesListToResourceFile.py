d530a8463816fe289ad8a302d5c409e731c862d8
mjferstl <39524509+mjferstl@users.noreply.github.com>
2019-06-21 10:36:10 +0200

    added informations about latitude and longitude to every city

--- openWeatherMapCitiesListToResourceFile.py
+++ openWeatherMapCitiesListToResourceFile.py
@@ -3,19 +3,26 @@
 import json
 
 cities = []
+lat_lon = []
 
 # read file
 with open('city.list.json', encoding='utf-8') as json_file:  
     data = json.load(json_file)
     for c in data:
     	if c['country'] == "DE":
-    		cities.append(c['name'] + ':' + str(c['id']))
+    		lat = c['coord']['lat']
+    		lon = c['coord']['lon']
+    		ll = '&lat=' + str(lat) + '&lon=' + str(lon)
+    		# only append, if not already existing
+    		if ll not in lat_lon:
+    			lat_lon.append(ll)
+    			cities.append('name=' + c['name'] + '&id=' + str(c['id']) + ll)
 
 # write file
-with open('openWeatherMapCityCodes.xml', 'w', encoding='utf-8') as f: 
+with open('openWeatherMapCities.xml', 'w', encoding='utf-8') as f: 
 	f.write('<?xml version="1.0" encoding="utf-8"?>\n')
 	f.write('<resources>\n')
-	f.write('\t<string-array name="owmCityCodex">\n') 
+	f.write('\t<string-array name="weatherMapCityCodes">\n') 
 	for city in cities:
 		f.write('\t\t<item>' + city + '</item>\n')
 	f.write('\t</string-array>\n')
