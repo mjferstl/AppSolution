c0e2495aa3251de0fbb38463b3602318a054864d
mjferstl <39524509+mjferstl@users.noreply.github.com>
2019-06-24 23:22:52 +0200

    added more information
    values of longitude and latitude added

--- openWeatherMapCitiesListToResourceFile.py
+++ openWeatherMapCitiesListToResourceFile.py
@@ -4,25 +4,25 @@ import json
 
 cities = []
 lat_lon = []
+separator = ':';
 
 # read file
 with open('city.list.json', encoding='utf-8') as json_file:  
     data = json.load(json_file)
     for c in data:
     	if c['country'] == "DE":
-    		lat = c['coord']['lat']
-    		lon = c['coord']['lon']
-    		ll = '&lat=' + str(lat) + '&lon=' + str(lon)
+    		lat, lon = c['coord']['lat'], c['coord']['lon']
+    		ll = separator + 'lat=' + str(lat) + separator + 'lon=' + str(lon)
     		# only append, if not already existing
     		if ll not in lat_lon:
     			lat_lon.append(ll)
-    			cities.append('name=' + c['name'] + '&id=' + str(c['id']) + ll)
+    			cities.append('name=' + c['name'] + separator + 'id=' + str(c['id']) + ll)
 
 # write file
 with open('openWeatherMapCities.xml', 'w', encoding='utf-8') as f: 
 	f.write('<?xml version="1.0" encoding="utf-8"?>\n')
 	f.write('<resources>\n')
-	f.write('\t<string-array name="weatherMapCityCodes">\n') 
+	f.write('\t<string-array name="weatherMapCities">\n') 
 	for city in cities:
 		f.write('\t\t<item>' + city + '</item>\n')
 	f.write('\t</string-array>\n')
